f69a82b9f732f409d4cd6201396485cfca5e30a6
Retky <jose.arredondo.a@outlook.com>
2022-05-17 10:06:38 -0500

    Modify 'index.js' to set StrictMode

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import store from './redux/configureStore';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-  ,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
