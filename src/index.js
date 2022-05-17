e891548cce28c80dd24cfc9db6806f3371f94fac
Retky <jose.arredondo.a@outlook.com>
2022-05-17 08:55:18 -0500

    Add 'store' confguration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <Provider store={store}>
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   ,
-  // </Provider>
 );
---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
+
+const reducer = combineReducers({
+
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk),
+);
+
+export default store;
